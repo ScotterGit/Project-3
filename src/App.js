f17cbe95a234158884646c54e8628923dbe9b766
ScotterGit <scooterboots45@gmail.com>
2021-01-23 09:50:44 -0600

    Attempting Toggling the <Todo /> templates

--- src/App.js
+++ src/App.js
@@ -25,9 +25,17 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter(task => id !== task.id);
-    setTasks(remainingTasks);
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
   const taskList = tasks.map(task => (
     <Todo
@@ -37,8 +45,15 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+ 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
 const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
